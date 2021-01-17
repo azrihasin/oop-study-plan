748f4dd4d2d0e293475b736b996a4aff42b8bed2
azri hasin <azrihasin1999@gmail.com>
2021-01-17 13:55:47 +0800

    Add files via upload

---
+++ studyplan/assets/js/script.js
@@ -0,0 +1,30 @@
+//Variables
+var overlay = $("#overlay"),
+        fab = $(".fab"),
+     cancel = $("#cancel"),
+     addsubject = $(".addsubject")
+     submit = $("#submit");
+
+//fab click
+fab.on('click', openFAB);
+addsubject.on('click', openFAB);
+overlay.on('click', closeFAB);
+cancel.on('click', closeFAB);
+
+function openFAB(event) {
+  if (event) event.preventDefault();
+  fab.addClass('active');
+  overlay.addClass('dark-overlay');
+
+}
+
+function closeFAB(event) {
+  if (event) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  fab.removeClass('active');
+  overlay.removeClass('dark-overlay');
+  
+}
