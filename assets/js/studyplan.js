dd9871c02c1a3b595367e3d2a5b41735b68878a6
azrihasin <48887150+azrihasin@users.noreply.github.com>
2021-02-26 15:47:13 +0800

    debugging excess subject

--- assets/js/studyplan.js
+++ assets/js/studyplan.js
@@ -1707,12 +1707,16 @@ function cookieManagement() {
 console.log("Cookies Saved");
 }
 
-function deleteAllCookies(){
+function deleteAllCookies() {
   var cookies = document.cookie.split(";");
-  for (var i = 0; i < cookies.length; i++)
-    deleteCookie(cookies[i].split("=")[0]);
-}
-
-function deleteCookie(name){
-  setCookie(name,"",-1);
+  for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i];
+      var eqPos = cookie.indexOf("=");
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + '=;' +
+          'expires=Thu, 01-Jan-1970 00:00:01 GMT;' +
+          'path=' + '/;' +
+          'domain=' + window.location.host + ';' +
+          'secure=;';
+  }
 }
